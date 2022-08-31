b38ec76166a47870af9c3b0b50df80ff219057cc
Efraim Alkhazov <efraimalkhazov@h-MacBook-Air-sl-idan.local>
2022-08-31 10:37:32 +0300

    fix: redux

--- src/components/api/methods.js
+++ src/components/api/methods.js
@@ -1,8 +1,6 @@
-import axios from 'axios';
 import NetInfo from '@react-native-community/netinfo';
 import {API} from './client';
 
-// check for connection
 instance.interceptors.request.use(async req => {
   const isConnected = await NetInfo.fetch().then(state => state.isConnected);
   if (isConnected) return req;
